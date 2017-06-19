5c9b545bb9c9f30254b6c5f8baf49e2ac5b8535e
MAJESTIC123 <karthiece996@gmail.com>
2017-06-19 11:55:35 +0530

    Update StringReverse.java

--- StringReverse.java
+++ StringReverse.java
@@ -1,15 +1,13 @@
 package logics;
 
 import java.util.Scanner;
-
-public class StringReverse {
-	public static void main(String[] args) {
-		
-		System.out.println("Enter the string");
-		Scanner s=new Scanner(System.in);
-		String st=s.next();
-		StringBuffer sb=new StringBuffer(st).reverse();
-		System.out.println("The reversed string is :"+sb);
-		
-	}
-}
+public class hh {
+	   public static void main(String args[]){
+		    String str[] = "hello world".split(" ");
+		    String finalStr="";
+		        for(int i = str.length-1; i>= 0 ;i--){
+		            finalStr += str[i]+" ";
+		        }
+		        System.out.println(finalStr);
+		    }
+		}
