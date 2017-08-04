759a39ed433df8098d5d66703e09c5cce4466893
MAJESTIC123 <karthiece996@gmail.com>
2017-08-04 14:27:28 +0530

    Add files via upload

---
+++ Hunter75.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Hunt 75  
+{
+	public static void main(String[] args)
+{
+
+	Scanner s=new Scanner(System.in);
+	System.out.println("The num of size array is ");
+	int n=s.nextInt();
+
+	int a[]=new int[n];
+	System.out.println("Enter the element array");
+	for(int i=0;i<n;i++)
+{
+		a[i]=s.nextInt();
+	}for(int i=0;i<n-1;i++)
+{
+		for(int j=i+1;j<n;j++)
+{
+			if(a[i]>a[j])
+{
+				System.out.print("\t"+a[j]);
+				break;
+			
+}                               else
+				System.out.print("\t-1");
+			break;
+		
+}
+	
+}
+                            System.out.print("\t-1");
+	
+}
+}
