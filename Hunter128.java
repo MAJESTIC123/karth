6dcc0363df4049e1b1f26775dfddc4357fb9c6b2
MAJESTIC123 <karthiece996@gmail.com>
2017-08-05 09:43:04 +0530

    Add files via upload

---
+++ Hunter128.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Hunter128 {
+public static void main(String[]args){
+	int a[]={1,2,3,4,5,6};
+	int sort[]=new int[a.length];
+	System.out.println(a.length);
+	int temp;
+	for(int i=0;i<a.length-2;i++){
+		for(int j=i+1;j<a.length-1;j++){
+			if(a[i]<a[j]){
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(int i=0;i<a.length-1;i++){
+		System.out.println(a[i]);
+	}
+	int j=a.length-1;
+		for(int i=0;i<(a.length/2);i++){
+			System.out.print(a[i]+" "+i+" ");
+			System.out.print(a[j]+" "+j+" ");
+			j--;
+		}
+	}
+}
