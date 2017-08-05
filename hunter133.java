7accbef4b8201653515bef55bf67959101cd07a0
MAJESTIC123 <karthiece996@gmail.com>
2017-08-05 09:49:58 +0530

    Update hunter133.java

--- hunter133.java
+++ hunter133.java
@@ -1,15 +1,20 @@
-package Hunter;
 import java.util.*;
-public class hunter133 {
-public static void main(String[] args) {
+public class hunt 133
+{
+public static void main(String[] args) 
+{
 	String input="Hello Word Is Input";
 	String str []=input.split(" ");
+	
 	String output="";
-	for(int i=str.length-1;i>=0;i--){
+	for(int i=str.length-1;i>=0;i--)
+	{
 	   output=output+str[i]+" ";
 	}
 	output=output.substring(0,output.length()-1);
 	output=output+".";
+	
+	
 	System.out.println(output);
 	
 }
